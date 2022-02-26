48da4e22dfcd431c245e23b72ddbc49313c49920
Alfreedo0 <97887629+Alfreedo0@users.noreply.github.com>
2022-02-26 13:43:42 +0300

    Add files via upload

---
+++ alfbot/index.js
@@ -0,0 +1,10 @@
+Alfbot
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
